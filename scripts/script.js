77344955f2712fa2b3e45bf86fa84d414b39138f
Deborah Lao <dlao7@my.bcit.ca>
2023-03-22 14:04:57 -0700

    shopping list demo

--- scripts/script.js
+++ scripts/script.js
@@ -18,32 +18,49 @@
 // let time = prompt("Enter what time of day it is, (day, night, afternoon): ")
 // greet(name_prompt, time)
 
-const radiusPara = document.querySelector("#radius")
-const resultPara = document.querySelector("#result")
+// const radiusPara = document.querySelector("#radius")
+// const resultPara = document.querySelector("#result")
 
-function calculateCircleArea(radius){
-    const area = Math.PI * radius ** 2;
-    return area
+// function calculateCircleArea(radius){
+//     const area = Math.PI * radius ** 2;
+//     return area
+// }
+
+// let math_input = prompt("Enter a radius: ");
+// radiusPara.textContent += math_input
+
+// if (isNaN(math_input)){
+//     resultPara.textContent = "The entered value is not a number.";
+// }
+
+// else{
+//     radiusPara.textContent = `The radius was ${math_input}.`;
+//     area = calculateCircleArea(math_input);
+//     area_round = area.toFixed(2);
+//     alert(`The area of a circle with radius ${math_input} is ${area_round}.`);
+//     resultPara.textContent = "There were no errors.";
+// }
+
+// make an array
+const shoppingList = ["apples", "eggs", "milk"];
+
+function populateList (shoppingList){
+// select the parent
+    const list = document.querySelector('.shopping')
+
+// make a loop so each item is added to the ul
+    shoppingList.forEach(element => {
+        const listItem = document.createElement('li');
+        listItem.textContent = element;
+        list.appendChild(listItem);
+    });
+    //alternative
+    // for (let item of shoppingList)
+    // for (let i = 0 ; i < shoppingList.length ; i++)
 }
 
-let math_input = prompt("Enter a radius: ");
-radiusPara.textContent += math_input
+populateList(shoppingList)
 
-if (isNaN(math_input)){
-    resultPara.textContent = "The entered value is not a number.";
-}
 
-else{
-    radiusPara.textContent = `The radius was ${math_input}.`;
-    area = calculateCircleArea(math_input);
-    area_round = area.toFixed(2);
-    alert(`The area of a circle with radius ${math_input} is ${area_round}.`);
-    resultPara.textContent = "There were no errors.";
-}
-
-// const groceryList = ["apples", "eggs", "milk"];
-// const shopList = document.querySelector("ul.shopping")
-
-// groceryList.forEach(element => shopList.append(<li>element</li>));
 
 
