e7ac59ba448aa34b3fd01c90c03671d580696eae
Deborah Lao <dlao7@my.bcit.ca>
2023-03-22 12:52:22 -0700

    Circle radius practice.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // function declaration: can be hoisted(used earlier in code)
 
-function greet(str, time = 'day'){
-    console.log(`Good ${time}, ${str}!`)
-}
+// function greet(str, time = 'day'){
+//     console.log(`Good ${time}, ${str}!`)
+// }
 
 // Function expression stored in variable:
 // let const = function(str){
@@ -14,7 +14,23 @@ function greet(str, time = 'day'){
 //     console.log(`Hello ${str}!`)
 // }
 
-let name_prompt = prompt("Enter your name: ");
-let time = prompt("Enter what time of day it is, (day, night, afternoon): ")
-greet(name_prompt, time)
+// let name_prompt = prompt("Enter your name: ");
+// let time = prompt("Enter what time of day it is, (day, night, afternoon): ")
+// greet(name_prompt, time)
 
+function calculateCircleArea(radius){
+    const area = Math.PI * radius ** 2
+    return area
+}
+
+let radius = prompt("Enter a radius: ");
+if (isNaN(radius)){
+    alert("That is not a number.");
+}
+
+else{
+area = calculateCircleArea(radius);
+area_round = area.toFixed(2);
+
+alert(`The area of a circle with radius ${radius} is ${area_round}.`);
+}
