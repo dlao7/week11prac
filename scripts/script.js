fbfc10afd5922dd792617856e00982b5f2096f7f
Deborah Lao <dlao7@my.bcit.ca>
2023-03-22 13:43:09 -0700

    Text content replacement.

--- scripts/script.js
+++ scripts/script.js
@@ -18,19 +18,32 @@
 // let time = prompt("Enter what time of day it is, (day, night, afternoon): ")
 // greet(name_prompt, time)
 
+const radiusPara = document.querySelector("#radius")
+const resultPara = document.querySelector("#result")
+
 function calculateCircleArea(radius){
-    const area = Math.PI * radius ** 2
+    const area = Math.PI * radius ** 2;
     return area
 }
 
-let radius = prompt("Enter a radius: ");
-if (isNaN(radius)){
-    alert("That is not a number.");
+let math_input = prompt("Enter a radius: ");
+radiusPara.textContent += math_input
+
+if (isNaN(math_input)){
+    resultPara.textContent = "The entered value is not a number.";
 }
 
 else{
-area = calculateCircleArea(radius);
-area_round = area.toFixed(2);
-
-alert(`The area of a circle with radius ${radius} is ${area_round}.`);
+    radiusPara.textContent = `The radius was ${math_input}.`;
+    area = calculateCircleArea(math_input);
+    area_round = area.toFixed(2);
+    alert(`The area of a circle with radius ${math_input} is ${area_round}.`);
+    resultPara.textContent = "There were no errors.";
 }
+
+// const groceryList = ["apples", "eggs", "milk"];
+// const shopList = document.querySelector("ul.shopping")
+
+// groceryList.forEach(element => shopList.append(<li>element</li>));
+
+
